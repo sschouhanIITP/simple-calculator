e2d795a2f80d1e764ac3206b70ee20d42988a796
Satyendra <sschouhan.cse@gmail.com>
2026-08-22 10:29:02 +0530

    add docstrings

--- calculator.py
+++ calculator.py
@@ -1,7 +1,9 @@
 def add(a, b):
+    """This function takes two numbers as input and returns their sum."""
     return a + b
 
 def subtract(a, b):
+    """This function takes two numbers as input and returns their difference."""
     return a - b
 
 def show_operations():
@@ -33,4 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
